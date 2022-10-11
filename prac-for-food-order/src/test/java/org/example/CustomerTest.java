e1bf73f3d08ce8400f5f8394e15ba96fb5061dd8
hyunsokstar <hyunsokstar@gmail.com>
2022-10-11 12:14:38 +0900

    update todo

--- prac-for-food-order/src/test/java/org/example/CustomerTest.java
+++ prac-for-food-order/src/test/java/org/example/CustomerTest.java
@@ -22,17 +22,6 @@ import static org.assertj.core.api.Assertions.assertThatCode;
  * 6. 구현
  */
 public class CustomerTest {
-
-    @DisplayName("메뉴 이름에 해당하는 요리를 주문 한다")
-    @Test
-    void orderTest() {
-        Customer customer = new Customer();
-        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
-        Cooking cooking = new Cooking();
-
-        assertThatCode(() -> customer.order("돈까스", menu, cooking))
-                .doesNotThrowAnyException();
-
-    }
+    
 
 }
